3dc1350d9f311457868eef80f8d33d9713825065
Giovannizt <giovanniagung@gmail.com>
2023-07-29 11:17:46 +0700

    PUSH INIT

---
+++ app/src/main/java/com/giovanni/banksampah/RiwayatActivity.kt
@@ -0,0 +1,11 @@
+package com.giovanni.banksampah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RiwayatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_riwayat)
+    }
+}
