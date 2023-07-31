c9cb73310b4dd8cf84f7e28aa2cc6032b4f18446
Agung Andra <dewaagung2002@gmail.com>
2023-07-31 21:16:11 +0700

    update UI Admin

---
+++ app/src/main/java/com/giovanni/banksampah/ui/admin/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.giovanni.banksampah.ui.admin.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.giovanni.banksampah.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.admin_activity_main)
+    }
+}
---
+++ app/src/main/java/com/giovanni/banksampah/ui/admin/riwayat/AdminRiwayatActivity.kt
@@ -0,0 +1,12 @@
+package com.giovanni.banksampah.ui.admin.riwayat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.giovanni.banksampah.R
+
+class AdminRiwayatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_riwayat)
+    }
+}
