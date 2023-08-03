5c19433f6023950f8d40a08dbf16c8a803fc4e4f
Agung Andra <dewaagung2002@gmail.com>
2023-08-03 20:58:12 +0700

    update UI

--- app/src/main/java/com/giovanni/banksampah/ui/user/main/MainActivity.kt
+++ app/src/main/java/com/giovanni/banksampah/ui/user/main/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.giovanni.banksampah.ui.user.main
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.giovanni.banksampah.databinding.ActivityMainBinding
 import com.giovanni.banksampah.ui.user.inputdata.InputDataActivity
@@ -13,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        setStatusBar()
         setNavigation()
         val userData = intent.getStringExtra(EXTRA_USER)
         binding.username.text = userData
@@ -34,5 +39,28 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         const val EXTRA_USER = "extra_user"
+    }
+
+    private fun setStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
+    companion object {
+        fun setWindowFlag(activity: Activity, bits: Int, on: Boolean) {
+            val window = activity.window
+            val layoutParams = window.attributes
+            if (on) {
+                layoutParams.flags = layoutParams.flags or bits
+            } else {
+                layoutParams.flags = layoutParams.flags and bits.inv()
+            }
+            window.attributes = layoutParams
+        }
     }
 }
