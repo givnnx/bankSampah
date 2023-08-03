43a391ec31eabfa68f84dae2c31984a234511399
Agung Andra <dewaagung2002@gmail.com>
2023-08-03 21:01:52 +0700

    update UI

--- app/src/main/java/com/giovanni/banksampah/ui/user/main/MainActivity.kt
+++ app/src/main/java/com/giovanni/banksampah/ui/user/main/MainActivity.kt
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setStatusBar()
         setNavigation()
         val userData = intent.getStringExtra(EXTRA_USER)
         binding.username.text = userData
@@ -39,28 +38,5 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         const val EXTRA_USER = "extra_user"
-    }
-
-    private fun setStatusBar() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        }
-        if (Build.VERSION.SDK_INT >= 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
-            window.statusBarColor = Color.TRANSPARENT
-        }
-    }
-
-    companion object {
-        fun setWindowFlag(activity: Activity, bits: Int, on: Boolean) {
-            val window = activity.window
-            val layoutParams = window.attributes
-            if (on) {
-                layoutParams.flags = layoutParams.flags or bits
-            } else {
-                layoutParams.flags = layoutParams.flags and bits.inv()
-            }
-            window.attributes = layoutParams
-        }
     }
 }
