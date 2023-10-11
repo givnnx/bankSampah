7f38f117ff8f9d0c37dc3569653be09b1d208762
Agung Andra <dewaagung2002@gmail.com>
2023-10-11 23:11:45 +0700

    Nambah UI

---
+++ app/src/main/java/com/giovanni/banksampah/activity_register_account.kt
@@ -0,0 +1,11 @@
+package com.giovanni.banksampah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_register_account : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_account)
+    }
+}
