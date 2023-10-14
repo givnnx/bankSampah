4dfe3a318e6db73d2ab464a4e3f4b3900564fd0f
Agung Andra <dewaagung2002@gmail.com>
2023-10-14 07:53:34 +0700

    Update UI

---
+++ app/src/main/java/com/giovanni/banksampah/TarikSaldoActivity.kt
@@ -0,0 +1,11 @@
+package com.giovanni.banksampah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TarikSaldoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tarik_saldo)
+    }
+}
